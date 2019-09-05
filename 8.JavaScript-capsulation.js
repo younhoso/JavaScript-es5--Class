318d6fd06d040511b871d419650e72fe17f710ef
younhoso <thdbsgh3443@naver.com>
2019-09-05 10:13:32 +0900

    190905 커밋

--- 8.JavaScript-capsulation.js
+++ 8.JavaScript-capsulation.js
@@ -1,13 +1,11 @@
 
 function Rectangle(width, height) {
-    this._width = width;
-    this._height = height;
+    this._width = width;     // 밑줄로 시작하는 변수는 절대 외부에서 건드리지 말자라는 약속!
+    this._height = height;   // 밑줄로 시작하는 변수는 절대 외부에서 건드리지 말자라는 약속!
 }
-
 Rectangle.prototype.getArea = function() {
     return this._width * this._height;
 };
-
 Rectangle.prototype.getWidth = function() {
     return this._width;
 };
@@ -21,8 +19,7 @@ Rectangle.prototype.setWidth = function(value) {
 Rectangle.prototype.setHeight = function(value) {
     if(value <= 0){return alert('0보다 작은 값을 넣을 수 없습니다.')}
     this._height = value;
-}
-
+};
 
 var tSet = new Rectangle(10, 5);
 console.log(tSet.getArea());
